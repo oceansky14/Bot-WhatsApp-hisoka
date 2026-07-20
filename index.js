ef682b7cf6c33a06f0b520c964b866839d0b8951
oceansky14 <dindaput0410@gmail.com>
2026-07-20 16:27:40 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: { 
+        args: ['--no-sandbox', '--disable-setuid-sandbox'] 
+    }
+});
+
+client.on('qr', (qr) => {
+    console.log('Scan QR ini:');
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Bot Hisoka Online!');
+});
+
+client.on('message', message => {
+    if(message.body === 'ping') {
+        message.reply('pong');
+    }
+});
+
+client.initialize();
